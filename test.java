ec75c831a60d411ec20dd71d13f42a184e66f9ac
Mamun Roshid <shajon.10@gmail.com>
2015-04-08 20:23:13 -0400

    changes made

---
+++ test.java
@@ -0,0 +1,11 @@
+package codelab;
+import java.util.Scanner;
+
+public class test {
+	 	public static void main(String [] args) {
+	 		Scanner stdin = new Scanner(System.in);
+	 		
+	 		int price = 999;
+	 		System.out.println((price/100) + " dollars and " + (price % 100) + " cents");	 		
+	 	}
+	 }
